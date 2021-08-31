8de064834e6673b581ac065f7f442b2f52df8a5d
izzkhair <izzkhair@gmail.com>
2021-09-01 04:42:35 +0800

    Completed Assignment

---
+++ hardwarezone/pipelines.py
@@ -0,0 +1,32 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+import pymongo
+
+
+class HardwarezonePipeline:
+    def __init__(self):
+        connection = pymongo.MongoClient(
+            "localhost",
+            27017
+        )
+        db = connection["hardwarezone"]
+        self.collection = db["threads"]
+
+    def process_item(self, item, spider):
+        valid = True
+        for data in item:
+            # If any of the item data is null, item will be drop
+            if item[data] is None:
+                valid = False
+                raise DropItem("Missing {0}!".format(data))
+        if valid:
+            # Added abit of data cleaning by trimming spaces, removing /n and removing /t for content data
+            item['content'] = item['content'].replace(
+                "\n", "").replace("\t", "").strip()
+            self.collection.insert(dict(item))
+            return item
